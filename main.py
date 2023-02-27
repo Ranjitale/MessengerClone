e2dabdd9babe2f2478ad5ec8f7572e8ce352707a
Ranjit Ale <120738349+Ranjitale@users.noreply.github.com>
2023-02-27 11:27:09 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+num1 = int(input("Enter the first number: "))
+num2= int(input("Enter the Second Number: "))
+print("Sum of the",num1,"and",num2," is ",num1+num2);
---
+++ script.js
@@ -0,0 +1,15 @@
+// const but1=document.getElementById("btn-1");
+// const but2=document.getElementById("btn-2");
+// const but3=document.getElementById("btn-3");
+// const but4=document.getElementById("btn-4");
+// function func1(){
+//     if(onclick=but1){
+//         but1.classList.add("active");
+//         but2.classList.remove("active");
+//         but3.classList.remove("active");
+//         but4.classList.remove("active");
+//     }
+//     // but2.classList.add("active");
+//     // but3.classList.add("active");
+//     // but4.classList.add("active");
+// }
